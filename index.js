049edfa569c17ae546a1f2dfd4deeb79b968f992
Mark Pundsack <markpundsack@users.noreply.github.com>
2016-07-08 13:05:48 -0700

    Move things to ./public

--- index.js
+++ index.js
@@ -7,6 +7,10 @@ app.use(express.static(__dirname + '/public'))
 app.get('/', function(request, response) {
   response.send('Hello World!')
 })
+app.get('/*', function(req, res){
+    var path = req.params[0] ? req.params[0] : 'index.html';
+    res.sendfile(path, {root: './public'});
+});
 
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
